c7105101436596b0791a1c1bf3df8cc2bc1cff1d
dominx99 <dominx992@gmail.com>
2023-05-13 19:00:54 +0200

    Hide sections that should not be visible for not logged user

--- src/cookery/sidebar/components/MenuSidebar.tsx
+++ src/cookery/sidebar/components/MenuSidebar.tsx
@@ -1,8 +1,10 @@
-import { ChevronLeft, FavoriteRounded, Publish, Receipt, Search } from "@mui/icons-material";
+import { ChevronLeft, FavoriteRounded, Logout, Publish, Receipt, Search } from "@mui/icons-material";
 import { Badge, Box, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, styled, SwipeableDrawer } from "@mui/material";
 import { useMemo } from "react";
-import { Link as RouteLink } from "react-router-dom";
+import { Link as RouteLink, useNavigate } from "react-router-dom";
 import BackofficeRouteList from "../../../backoffice/router/BackofficeRouteList";
+import AuthenticationRouteList from "../../../security/app/routes/AuthenticationRouteList";
+import { authenticationDetails, setAuthenticationDetails } from "../../../security/authentication/api/AuthenticationSlice";
 import { useAppDispatch, useAppSelector } from "../../../shared/app/hooks";
 import CookeryRouteList from "../../app/router/CookeryRouteList";
 import { favoriteRecipesSelectors } from "../../favorite-recipes/api/FavoriteRecipesSlice";
@@ -19,6 +21,8 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 export default function MenuSidebar() {
   const dispatch = useAppDispatch();
   const isOpen = useAppSelector(isMenuSidebarOpen);
+  const details = useAppSelector(authenticationDetails);
+  const navigate = useNavigate();
 
   const handleDrawerClose = () => {
     dispatch(toggleMenuSidebar());
@@ -29,6 +33,21 @@ export default function MenuSidebar() {
   const favoriteRecipesCount: number = useMemo(() => {
     return favoriteRecipes.length
   }, [favoriteRecipes])
+
+  const isAuthenticated = () => {
+    return details !== null;
+  }
+
+  const isBackofficeUser = () => {
+    return details?.roles.includes('ROLE_BACKOFFICE');
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('AUTHENTICATION_DETAILS');
+    dispatch(setAuthenticationDetails(null));
+
+    navigate(AuthenticationRouteList.AUTHENTICATE);
+  }
 
   return (
     <SwipeableDrawer
@@ -62,20 +81,32 @@ export default function MenuSidebar() {
             </ListItemButton>
           </ListItem>
 
-          <ListItem disablePadding>
-            <ListItemButton component={RouteLink} to={CookeryRouteList.MY_RECIPES} color="inherit" onClick={() => dispatch(toggleMenuSidebar())}>
-              <ListItemIcon><Receipt /></ListItemIcon>
-              <ListItemText primary="My recipes" />
-            </ListItemButton>
-          </ListItem>
+          {isAuthenticated() && (
+            <ListItem disablePadding>
+              <ListItemButton component={RouteLink} to={CookeryRouteList.MY_RECIPES} color="inherit" onClick={() => dispatch(toggleMenuSidebar())}>
+                <ListItemIcon><Receipt /></ListItemIcon>
+                <ListItemText primary="My recipes" />
+              </ListItemButton>
+            </ListItem>
+          )}
 
-          <ListItem disablePadding>
-            <ListItemButton component={RouteLink} to={BackofficeRouteList.PUBLISH_RECIPES_REQUESTS} color="inherit" onClick={() => dispatch(toggleMenuSidebar())}>
-              <ListItemIcon><Publish /></ListItemIcon>
-              <ListItemText primary="Publish requests" />
-            </ListItemButton>
-          </ListItem>
+          {isAuthenticated() && isBackofficeUser() && (
+            <ListItem disablePadding>
+              <ListItemButton component={RouteLink} to={BackofficeRouteList.PUBLISH_RECIPES_REQUESTS} color="inherit" onClick={() => dispatch(toggleMenuSidebar())}>
+                <ListItemIcon><Publish /></ListItemIcon>
+                <ListItemText primary="Publish requests" />
+              </ListItemButton>
+            </ListItem>
+          )}
 
+          {isAuthenticated() && (
+            <ListItem disablePadding>
+              <ListItemButton color="inherit" onClick={handleLogout}>
+                <ListItemIcon><Logout /></ListItemIcon>
+                <ListItemText primary="Logout" />
+              </ListItemButton>
+            </ListItem>
+          )}
         </List>
       </Box>
     </SwipeableDrawer>
